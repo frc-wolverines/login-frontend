e993cb951dcb2011c91f2619a1d861f0b77b4ad0
frc-wolverines <rosemont5274@gmail.com>
2024-09-08 21:31:14 -0700

    I think this is the init commit (?)

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:login/page/roster.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  MainApp({super.key});
-
-  bool removed = false;
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(colorScheme: const ColorScheme.dark(primary: Colors.white)),
-      home: Roster()
+      home: Column()
     );
   }
-
-  static void doNothing() {}
-
-  static BoxDecoration cardBoxDeco(bool removed) {
-  if(removed == true) {
-    return BoxDecoration(
-      color: const Color.fromARGB(26, 198, 0, 0),
-      border: Border.all(
-        width: 0,
-        color: const Color.fromARGB(26, 101, 101, 101)
-      ),
-      borderRadius: BorderRadius.circular(12.5)
-    );
-  }
-
-  return BoxDecoration(
-    color: const Color.fromARGB(26, 101, 101, 101),
-    border: Border.all(
-      width: 0,
-      color: const Color.fromARGB(26, 101, 101, 101)
-    ),
-    borderRadius: BorderRadius.circular(12.5)
-  );
-}
 }
---
+++ lib/shared/theme.dart
@@ -0,0 +1,17 @@
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData themeData = ThemeData(
+
+  );
+}
+
+class AppColors {
+
+  static Color textPrimary = const Color.fromARGB(255, 255, 255, 255);
+  static Color textSecondary = const Color.fromARGB(155, 255, 255, 255);
+
+}
